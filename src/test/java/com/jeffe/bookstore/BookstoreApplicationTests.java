6ab91259dd3bfb05f7195b989e74a7d237827fcc
JeffeDev <jeffe.info@gmail.com>
2021-10-16 00:45:33 -0300

    :faet: Commit Inicial

---
+++ src/test/java/com/jeffe/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffe.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
